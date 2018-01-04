64d4d30efe4e9ae57dcd37c5d71f599a610cb471
lite12 <34488369+lite12@users.noreply.github.com>
2018-01-04 14:55:03 -0600

    Update themes.php

--- includes/themes.php
+++ includes/themes.php
@@ -1,5 +1,8 @@
 <?
-	if (!isset($_SESSION)) { session_start(); }
+	// Randomizes theme elements.
 	$th = array('eldeon','junon','eldeon'); 
+	if (!isset($_SESSION)) { 
+		session_start(); 
+	}
 	$_SESSION['th'] = $th[array_rand($th, 1)];
 ?>
